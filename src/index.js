845f0c18744ffaab4c0ee03a43672a0f7c8da34e
Daniele Di Salvo <disi91@hotmail.it>
2023-04-13 19:58:15 +0200

    ADD sass to Project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Menu from "./components/Menu";
-import data from "../data/recipes.json"
+import data from "../data/recipes.json";
+import "bootstrap/scss/bootstrap.scss";
+import "../scss/style.css";
 
 ReactDOM.render(<Menu recipes={data} />, document.getElementById("root"));
