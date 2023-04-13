b4075e6b2ba78fe2618c2fecaff6546b634ef484
Daniele Di Salvo <disi91@hotmail.it>
2023-04-13 20:51:12 +0200

    UPDATE bundle.js, style.scss, style.css

--- dist/assets/bundle.js
+++ dist/assets/bundle.js
@@ -228,7 +228,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "body {\n  background-color: #e9f5fe;\n}\n\n", "",{"version":3,"sources":["webpack://./scss/style.css"],"names":[],"mappings":"AAAA;EACE,yBAAA;AACF","sourcesContent":["body {\n  background-color: #e9f5fe;\n}\n\n/*# sourceMappingURL=style.css.map */\n"],"sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, "body {\n  background-color: #e9f5fe;\n}\n\n#root {\n  padding-left: 25px;\n}\n\n", "",{"version":3,"sources":["webpack://./scss/style.css"],"names":[],"mappings":"AAAA;EACE,yBAAA;AACF;;AAEA;EACE,kBAAA;AACF","sourcesContent":["body {\n  background-color: #e9f5fe;\n}\n\n#root {\n  padding-left: 25px;\n}\n\n/*# sourceMappingURL=style.css.map */\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
